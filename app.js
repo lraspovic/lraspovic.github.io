95fb1d52695330814706e33f0a362cb36777cf22
lraspovic <55063375+lraspovic@users.noreply.github.com>
2020-08-27 22:24:30 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const logo = document.querySelectorAll('#logo path');
-
-console.log(logo);
-
-let i = 0;
-logo.forEach( node => {
-    console.log(`Letter ${i} is ${node.getTotalLength()}`);
-    i++;
-})
